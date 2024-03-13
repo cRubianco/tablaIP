139a09f11dd7cca362e7a5ab554e4a47bd29aa3e
sintornillos <rubiancocarlos@gmail.com>
2024-03-13 20:45:34 -0300

    inicio frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SimpleTable from './components/SimpleTable'
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Table</h1>
+      <SimpleTable />
+    </div>
+  )
+}
+
+export default App
